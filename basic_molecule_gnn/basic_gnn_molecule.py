926f2e1a8179c85509d684f72f9b585f2cd65afb
Joseph Byun <jobyun45@gmail.com>
2026-02-18 14:40:53 -0800

    slight fixes to models

--- basic_molecule_gnn/basic_gnn_molecule.py
+++ basic_molecule_gnn/basic_gnn_molecule.py
@@ -1,5 +1,13 @@
 import torch
 from torch_geometric.datasets import MoleculeNet
+from torch_geometric.loader import DataLoader
+from sklearn.model_selection import train_test_split
+from tqdm import tqdm
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv, global_mean_pool
+
+
 
 dataset = MoleculeNet(root ="data", name = "ESOL")
 print(dataset)
@@ -10,17 +18,7 @@ print("Number of node features: ", dataset.num_node_features)
 data = dataset[0]
 print("item structure: ", data)
 
-from torch.utils.data import random_split
-
 torch.manual_seed(42)
-
-train_size = int(0.8 * len(dataset))
-test_size = len(dataset) - train_size
-
-train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
-
-from torch_geometric.loader import DataLoader
-from sklearn.model_selection import train_test_split
 
 train_idx, temp_idx = train_test_split(
     list(range(len(dataset))),
@@ -35,15 +33,8 @@ val_idx, test_idx = train_test_split(
 )
 
 
-train_loader = DataLoader(dataset[train_idx], batch_size = 32, shuffle = True)
-val_loader = DataLoader(dataset[val_idx], batch_size = 32)
-test_loader = DataLoader(dataset[test_idx], batch = 32)
 
 
-
-import torch.nn as nn
-import torch.nn.functional as F
-from torch_geometric.nn import GCNConv, global_mean_pool
 
 class GCNModel(nn.Module):
     def __init__(self, in_channels, hidden_dim):
@@ -53,22 +44,23 @@ class GCNModel(nn.Module):
         self.conv2 = GCNConv(hidden_dim, hidden_dim)
         self.lin = nn.Linear(hidden_dim, 1)
 
-    def forward(self, x, edge_index, batch):
+    def forward(self, data):
         # Message passing
         x, edge_index, batch = data.x, data.edge_index, data.batch
 
         x = self.conv1(x, edge_index)
         x = F.relu(x)
         x  = self.conv2(x, edge_index)
+        x = F.relu(x)
 
         x = global_mean_pool(x, batch)
-
         x = self.lin(x)
         
         return x.squeeze()
     
 
 from torch_geometric.nn import GATConv
+
 class GATModel(torch.nn.Module):
     def __init__(self, in_channels, hidden_dim):
         super().__init__()
@@ -81,23 +73,22 @@ class GATModel(torch.nn.Module):
         x, edge_index, batch = data.x, data.edge_index, data.batch
 
         x = self.conv1(x, edge_index)
-        x = F.elu()
+        x = F.elu(x)
         x = self.conv2(x, edge_index)
         x = F.elu(x)
 
         x = global_mean_pool(x, batch)
         x = self.lin(x)
-        return x.squeeze()
+        return x.squeeze()  
 
-
-from tqdm import tqdm
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-model = GCNModel(dataset.num_node_features).to(device)
-# model = GATModel(dataset.num_node_features).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr = 0.001)
-criterion = nn.MSELoss()
+# Model Orchestration
+def get_model(model_name, in_channels, hidden_dim=64):
+    if model_name == "GCN":
+        return GCNModel(in_channels, hidden_dim)
+    if model_name == "GAT":
+        return GATModel(in_channels, hidden_dim)
+    else:
+        raise ValueError("Model Name NOT Found")
 
 def train(model, loader, optimizer, criterion):
     model.train()
@@ -108,7 +99,7 @@ def train(model, loader, optimizer, criterion):
         optimizer.zero_grad()
 
         out = model(data)
-        loss = criterion(out, data.y)
+        loss = criterion(out, data.y.view(-1))
         loss.backward()
         optimizer.step()
 
@@ -116,74 +107,87 @@ def train(model, loader, optimizer, criterion):
 
     return total_loss / len(loader)
 
-# def test(loader):
-#     model.eval()
-#     total_error = 0
-
-#     with torch.no_grad():
-#         for data in loader:
-#             data = data.to(device)
-#             out = model(data.x, data.edge_index, data.batch)
-#             total_error += torch.abs(out - data.y).sum().item()
-
-#     return total_error / len(loader.dataset)
-
 from sklearn.metrics import mean_absolute_error
 
 @torch.no_grad()
-def evaluate(model, loader):
+def evaluate(model, loader, device):
     model.eval()
     preds = []
     targets = []
 
     for data in loader:
+        data = data.to(device)
         out = model(data)
         preds.append(out.cpu())
-        targets.append(data.y.cpu())
+        targets.append(data.y.view(-1).cpu())
     
     preds = torch.cat(preds)
     targets = torch.cat(targets)
 
     mae = mean_absolute_error(targets, preds)
-    return mae, preds, targets
+
+    return mae
 
 import copy
 
-best_val_mae = float('inf')
-best_model_state = None
-patience = 30
-counter = 0
-
-for epoch in range(300):
-    loss = train(model, train_loader, optimizer, criterion)
-    val_mae, _, _ = evaluate(model, val_loader)
-
-    if epoch % 10 == 0:
-        print(f"Epoch {epoch:03d} | Loss: {loss:0.4f}  |  Validation MAE: {val_mae:0.4f}")
+def run_experiment(model_name, device):
     
-    if val_mae < best_val_mae:
-        best_val_mae = val_mae
-        best_model_state = copy.deepcopy(model.state_dict())
-        counter = 0
-    else:
-        counter += 1
-    
-    if counter >= patience:
-        print("Early stopping triggers")
-        break
+    train_loader = DataLoader(dataset[train_idx], batch_size = 32, shuffle = True)
+    val_loader = DataLoader(dataset[val_idx], batch_size = 32)
+    test_loader = DataLoader(dataset[test_idx], batch_size = 32)
 
-model.load_state_dict(best_model_state)
+    model = get_model(model_name, dataset.num_node_features).to(device)
+    optimizer = torch.optim.Adam(model.parameters(), lr = 0.001, weight_decay = 1e-4)
+    criterion = nn.MSELoss()
 
-test_mae, test_preds, test_targets = evaluate(model, test_loader)
 
-print(f"Final Test Mae: {test_mae:0.4f}")
+    best_val = float('inf')
+    best_model_state = None
+    patience = 30
+    counter = 0
 
-import matplotlib.pyplot as plt
+    for epoch in range(300):
+        loss = train(model, train_loader, optimizer, criterion)
+        val_mae, _, _ = evaluate(model, val_loader)
 
-plt.scatter(test_targets, test_preds)
-plt.xlabel("True")
-plt.ylabe("Predicted")
-plt.plot([min(test_targets), max(test_targets)],
-         [min(test_targets), max(test_targets)],
-         color = "red")
-plt.show()
+        if epoch % 10 == 0:
+            print(f"Epoch {epoch:03d} | Loss: {loss:0.4f}  |  Validation MAE: {val_mae:0.4f}")
+        
+        if val_mae < best_val:
+            best_val = val_mae
+            best_model_state = copy.deepcopy(model.state_dict())
+            counter = 0
+        else:
+            counter += 1
+        
+        if counter >= patience:
+            print("Early stopping triggers")
+            break
+
+    model.load_state_dict(best_model_state)
+
+    test_mae = evaluate(model, test_loader)
+    return best_val, test_mae
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+models = ["GCN", "GAT"]
+
+for model_name in models:
+    val_mae, test_mae = run_experiment(model_name, device)
+
+    print("-" * 30)
+    print(f"Model: {model_name}")
+    print(f"Best Validation MAE: {val_mae:.4f}")
+    print(f"Test MAE: {test_mae:.4f}")
+
+
+# import matplotlib.pyplot as plt
+
+# plt.scatter(test_targets, test_preds)
+# plt.xlabel("True")
+# plt.ylabel("Predicted")
+# plt.plot([min(test_targets), max(test_targets)],
+#          [min(test_targets), max(test_targets)],
+#          color = "red")
+# plt.show()
